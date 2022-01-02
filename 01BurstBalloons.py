d5afd23ab555f3c370a46dc70c52aa8e5101301b
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2022-01-02 10:27:41 +0530

    Create 01BurstBalloons.py

---
+++ 01BurstBalloons.py
@@ -0,0 +1,18 @@
+# Question: 
+
+# Solution:
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums = [1]+[n for n in nums if n!=0]+[1]
+        regional_max_coins = [[0 for i in range(len(nums))] for j in range(len(nums))]
+        for balloons_to_burst in range(1, len(nums)-1): # number of balloons in (l,r) region
+            for l in range(0, len(nums)-balloons_to_burst-1): # for m and r to be assigned legally
+                r = l+balloons_to_burst+1
+                for m in range(l+1,r):
+                    regional_max_coins[l][r] = max(regional_max_coins[l][r], regional_max_coins[l][m]+regional_max_coins[m][r]+nums[l]*nums[m]*nums[r])
+        return regional_max_coins[0][-1]
+
+# Verdict:
+# Runtime: 8728 ms, faster than 53.84% of Python3 online submissions for Burst Balloons.
+# Memory Usage: 20 MB, less than 44.14% of Python3 online submissions for Burst Balloons.
