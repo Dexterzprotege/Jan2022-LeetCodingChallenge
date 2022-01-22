54a519e397a8882d61add6ad5fc91b645a73d037
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2022-01-22 23:14:24 +0530

    Update 22StoneGameIV.py

--- 22StoneGameIV.py
+++ 22StoneGameIV.py
@@ -1,8 +1,61 @@
 # Question: https://leetcode.com/problems/stone-game-iv/
 
-# Soltuion:
+# ----------------------------------------------------------------------------------------- #
+
+# Solution 3:
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
+        dp = [0]*100001
+        @lru_cache(None)
+        def helper(n):
+            if n <= 0:
+                return False
+            if dp[n] != 0:
+                return dp[n]
+            for i in range(0, n+1):
+                if dp[i]:
+                    continue
+                k = 1
+                while i + k * k <= n:
+                    dp[i+k*k] = True
+                    k += 1
+            return dp[n]
+        return helper(n)    
+    
+# Verdict:
+# Runtime: 272 ms, faster than 86.83% of Python3 online submissions for Stone Game IV.
+# Memory Usage: 15 MB, less than 59.88% of Python3 online submissions for Stone Game IV.
+
+# ----------------------------------------------------------------------------------------- #
+
+# Solution 2:
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        dp = [-1]*100001
+        def helper(n):
+            if n <= 0:
+                return 0
+            if dp[n] != -1:
+                return dp[n]
+            i = 1
+            while i*i <= n:
+                if helper(n-i*i) == 0:
+                    dp[n] = 1
+                    return 1
+                i += 1
+            return 0
+        return helper(n)    
+
+# Verdict:
+# Runtime: 4027 ms, faster than 18.56% of Python3 online submissions for Stone Game IV.
+# Memory Usage: 164.2 MB, less than 9.58% of Python3 online submissions for Stone Game IV.
+
+# ----------------------------------------------------------------------------------------- #
+
+# Soltuion 1:
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        @lru_cache(None)
         def helper(n):
             if n <= 0:
                 return 0
@@ -15,4 +68,7 @@ class Solution:
         return helper(n)
 
 # Verdict:
-TLE
+# Runtime: 4595 ms, faster than 16.77% of Python3 online submissions for Stone Game IV.
+# Memory Usage: 161 MB, less than 9.58% of Python3 online submissions for Stone Game IV.
+
+# ----------------------------------------------------------------------------------------- #
