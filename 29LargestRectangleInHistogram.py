1fe82781af0954b57c74368add069fb7ca30706f
Sharath Kamlekar <42378552+Dexterzprotege@users.noreply.github.com>
2022-01-29 23:30:51 +0530

    Update 29LargestRectangleInHistogram.py

--- 29LargestRectangleInHistogram.py
+++ 29LargestRectangleInHistogram.py
@@ -1,4 +1,6 @@
-# Question:
+# Question: https://leetcode.com/problems/largest-rectangle-in-histogram/
+
+# ------------------------------------------------------------------------------------------------------------- #
 
 # Solution 2: Stack: Find min on left and right:
 class Solution:
@@ -33,6 +35,30 @@ class Solution:
 # Runtime: 1562 ms, faster than 9.43% of Python3 online submissions for Largest Rectangle in Histogram.
 # Memory Usage: 28.4 MB, less than 29.33% of Python3 online submissions for Largest Rectangle in Histogram.
 
+# ------------------------------------------------------------------------------------------------------------- #
+
 # Solution 1: Iterative: Find min on left and right:
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        area = 0
+        for i in range(n):
+            p1 = i-1 # First smaller point on the left
+            p2 = i+1 # First smaller point on the right
+            while p1 >= 0:
+                if heights[p1] < heights[i]:
+                    break
+                p1 -= 1
+            while p2 <= n-1:
+                if heights[p2] < heights[i]:
+                    break
+                p2 += 1
+            width = p2 - p1 -1
+            height = heights[i]
+            area = max(area, width*height)
+        return area
 
 # Verdict
+TLE
+
+# ------------------------------------------------------------------------------------------------------------- #
